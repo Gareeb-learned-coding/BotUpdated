d5722ad26749587fc3f817299765ea36c71d589a
Gareeb-learned-some-coding <83328033+Gareeb-learned-coding@users.noreply.github.com>
2022-03-09 12:16:38 +0530

    Update run.py

--- bot/run.py
+++ bot/run.py
@@ -1,4 +1,7 @@
 import os
+#import pynacl
+#import dnspython
+import server
 
 from bot import build_bot
 
@@ -9,3 +12,5 @@ if os.name != "nt":
 
 if __name__ == "__main__":
     build_bot().run()
+
+server.server()
