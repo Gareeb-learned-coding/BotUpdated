0ed3e89572e7dad988bd495a88b54b91b77ba6bb
Gareeb-learned-some-coding <83328033+Gareeb-learned-coding@users.noreply.github.com>
2022-03-09 12:18:42 +0530

    Create bot.py

---
+++ bot/bot.py
@@ -0,0 +1,29 @@
+import hikari
+import tanjun
+import os
+
+def build_bot() -> hikari.GatewayBot:
+    TOKEN = os.getenv("DISCORD_TOKEN")
+    bot = hikari.GatewayBot(TOKEN)
+    make_client(bot)
+
+    return bot
+
+
+def make_client(bot: hikari.GatewayBot) -> tanjun.Client:
+    client = (
+        tanjun.Client.from_gateway_bot(
+            bot,
+            mention_prefix=True,
+            declare_global_commands=931146494164205588
+        )
+    ).add_prefix("!")
+
+    client.load_modules("plugins.begin")
+    client.load_modules("plugins.add")
+    client.load_modules("plugins.remove")
+    client.load_modules("plugins.tracks")
+    client.load_modules("plugins.link")
+    client.load_modules("plugins.help")
+    
+    return client
